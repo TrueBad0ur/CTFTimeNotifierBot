f5a80e28ca708220342e7f3759f89297ef3eabde
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-02-23 11:42:56 -0500

    Update

--- bot.py
+++ bot.py
@@ -8,10 +8,17 @@ import requests
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import datetime
 from dateutil import tz
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hello here!\nI am a ctftime notifier bot!')
+    update.message.reply_text('Hello here!\nI am a ctftime notifier bot!\n/getlist - get current list of CTFs\n')
 
 def parseTime(time_str):
     from_zone = tz.gettz('UTC')
@@ -59,32 +66,18 @@ def error(update, context):
 
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(token, use_context=True)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("getlist", getlist))
     dp.add_handler(CommandHandler("help", help))
 
-    # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
 
-    # log all errors
     dp.add_error_handler(error)
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
