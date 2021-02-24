1ae0932d0da70b6a899a4991d2ba5b9fa993b706
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-02-24 11:56:05 +0000

    Delete reply func

--- bot.py
+++ bot.py
@@ -75,7 +75,8 @@ def main():
     dp.add_handler(CommandHandler("getlist", getlist))
     dp.add_handler(CommandHandler("help", help))
 
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # answer reply to user
+    #dp.add_handler(MessageHandler(Filters.text, echo))
 
     dp.add_error_handler(error)
     updater.start_polling()
